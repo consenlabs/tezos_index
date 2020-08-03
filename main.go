09fb24484de8c337b11e80ba20dee16c3aa296d2
sandy <18382255942@163.com>
2020-08-03 18:18:21 +0800

    feat(): add

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/zyjblockchain/sandy_log/log"
+	"tezos_index/models"
+)
+
+func main() {
+	// 0. 初始化日志级别、格式、是否保存到文件
+	log.Setup(log.LevelDebug, false, true)
+
+	dsn := "tac_user:NwHJhkcTKHmDr2RZ@tcp(223.27.39.183:3306)/tezos_index?charset=utf8mb4&parseTime=True&loc=Local"
+	models.InitDB(dsn)
+
+}
